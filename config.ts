9f3cb75d682c24375a488d74703a81c49644e112
Bytecoder <bytecoder0x@gmail.com>
2023-05-03 20:02:51 +0300

    feat: config with starknet addresses

---
+++ config.ts
@@ -0,0 +1,17 @@
+import "dotenv/config";
+
+export const config = {
+    starknet: { sequencerUrl: "https://alpha-mainnet.starknet.io", rpcUrl: process.env.STARKNET_RPC_URL || "" },
+    tokens: {
+        ETH: "0x049d36570d4e46f48e99674bd3fcc84644ddd6b96f7c741b1562b82f9e004dc7",
+        USDC: "0x053c91253bc9682c04929ca02ed00b3e423f6710d2ee7e0d5ebb06f3ecf368a8",
+        USDT: "0x068f5c6a61780768455de69077e07e89787839bf8166decfbf92b645209c0fb8",
+    },
+    dex: {
+        jediswap: "0x041fd22b238fa21cfcf5dd45a8548974d8263b3a531a60388411c5e230f97023",
+    },
+    argent: {
+        proxyClassHash: "0x025ec026985a3bf9d0cc1fe17326b245dfdc3ff89b8fde106542a3ea56c5a918",
+        accountClassHash: "0x033434ad846cdd5f23eb73ff09fe6fddd568284a0fb7d1be20ee482f044dabe2",
+    },
+};
